1b502baded01250c4429dff3ebc222264a5af117
光鱼 <guang.yu@yingdao.com>
2025-05-14 14:34:21 +0800

    feat: Chapter 加点动画

--- src/components/PreviewCard/components/Chapter.tsx
+++ src/components/PreviewCard/components/Chapter.tsx
@@ -1,50 +1,69 @@
-import { Button } from 'antd';
-import classNames from 'classnames';
+import { Button } from "antd";
+import classNames from "classnames";
+import { motion } from "framer-motion";
 
-export default function Chapter(props: { data: ChapterStep; jump?: (uid: string) => void; className?: string }) {
+export default function Chapter(props: {
+  data: ChapterStep;
+  jump?: (uid: string) => void;
+  className?: string;
+}) {
   const { title, align, subtitle, actions } = props.data;
   return (
     <div
       className={classNames(
-        ' relative backdrop-blur-md bg-[#ffffffb3] w-full h-full rounded-md overflow-hidden',
-        props.className,
+        " relative backdrop-blur-md bg-[#ffffffb3] w-full h-full rounded-md overflow-hidden",
+        props.className
       )}
     >
       <div className="h-full  flex flex-col justify-center px-10 gap-2">
-        <div
+        <motion.div
           className="p-0 text-2xl font-semibold"
           style={{
             textAlign: align,
           }}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
         >
           {title}
-        </div>
-        <div
+        </motion.div>
+        <motion.div
           className="p-0 whitespace-break-spaces text-sm mb-1"
           style={{
             textAlign: align,
           }}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3, ease: "easeInOut", delay: 0.1 }}
         >
           {subtitle}
-        </div>
+        </motion.div>
 
-        <div
+        <motion.div
           className="flex gap-2"
           style={{
             justifyContent: align,
           }}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3, ease: "easeInOut", delay: 0.2 }}
         >
           {actions.map((it, index) => {
-            if (it.type === 'button') {
+            if (it.type === "button") {
               return (
-                <Button size="large" type="primary" key={index} onClick={() => props.jump?.(it.destination)}>
+                <Button
+                  size="large"
+                  type="primary"
+                  key={index}
+                  onClick={() => props.jump?.(it.destination)}
+                >
                   <div className="px-6 text-white text-center">{it.text}</div>
                 </Button>
               );
             }
-            return '暂不支持类型:' + it.type;
+            return "暂不支持类型:" + it.type;
           })}
-        </div>
+        </motion.div>
       </div>
     </div>
   );
