48321e4fa73fcdab31bbbc98408c640002fad590
光鱼 <guang.yu@yingdao.com>
2025-08-19 19:45:12 +0800

    fix: 统一代码风格，调整引号和缩进格式

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,16 +1,20 @@
-import { getAccessToken } from '@/api';
-import CustomLoading from '@/components/CustomLoading';
-import { AuthLoginLayout } from '@/Layouts/AuthLogin';
-import { useMount } from 'ahooks';
+import { getAccessToken } from "@/api";
+import CustomLoading from "@/components/CustomLoading";
+import { AuthLoginLayout } from "@/Layouts/AuthLogin";
+import { useMount } from "ahooks";
 
 export default function AuthPage() {
   useMount(() => {
-    window.open(`tourbitwebauth://auth?token=${getAccessToken()}`, '_self');
+    window.open(`tourbitwebauth://auth?token=${getAccessToken()}`, "_self");
   });
+
   return (
     <AuthLoginLayout>
-      <div className=" relative flex items-center justify-center h-[100vh]">
-        <CustomLoading />
+      <div className=" relative flex items-center justify-center h-[100vh] ">
+        <div className="flex flex-col justify-center items-center">
+          <CustomLoading />
+          <div className="mt-20 text-center">{getAccessToken()}</div>
+        </div>
       </div>
     </AuthLoginLayout>
   );
